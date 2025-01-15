f8ab005c8054de791d18d588e5fb0fc120a098fb
Samirghimire-5 <samirghimire9841@gmail.com>
2025-01-15 19:09:19 +0545

    code

---
+++ practice2.js
@@ -0,0 +1,61 @@
+// Q. Swap Two Numbers
+// let a = 5;
+// let b = 10;
+// let c = a;
+// a = b;
+// b = c;
+// console.log(a, b)
+// Output: a = 10, b = 5
+
+
+// Q. Even or Odd
+// let value = 3;
+// if (value % 2 === 0) {
+//     console.log('even');
+// } else {
+//     console.log('odd');
+// }
+
+
+
+// Q. Prime Numbers (1 to 50)
+// for (let i=2; i<=10; i++) {
+//     let prime = true;
+//     for(let j=2; j<i; j++){
+//         if(i%j === 0) {
+//             prime = false;
+//         }
+//     }
+//     if (prime) {
+//         console.log(i)
+//     }
+// }
+
+
+// Q. Largest Number in an Array
+// let arr = [10, 45, 2, 100, 67];
+// let max = 0;
+// for (val of arr) {
+//     if (max < val) {
+//         max = val;
+//     }
+// }
+// console.log(max);
+
+
+// Q. Object Property Count
+// const people = { name: "Alice", age: 25, city: "NY" }
+// let prop = Object.keys(people)
+// console.log(prop.length)
+
+
+// Q. Factorial
+// function factorial(num) {
+//     let fac = 1;
+//     for(let i=1; i<=num; i++) {
+//         fac *= i;
+//     }
+//     console.log(fac)
+// }
+// factorial(10)
+
